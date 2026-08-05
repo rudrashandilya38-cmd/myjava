30785996d93bb949e43a30703c916337a01e6a96
rudrashandilya38-cmd <rudrashandilya38@gmail.com>
2026-08-05 16:53:14 +0000

    part 1

--- basics/air.js
+++ basics/air.js
@@ -1,1 +1,11 @@
-console.log("Rudra")
+const accountId = 456
+let accountEmail="user@gm.com"
+let accountPassword="123456"
+let accountCity="patna"
+
+// accountId=321
+// Once constant variable is assigned a value,it cannot be reassigned.This is an error.
+accountEmail="didi.com"
+acccountPassword="12345678"
+accountCity="delhi"
+console.table([accountId, accountEmail, accountPassword, accountCity])
