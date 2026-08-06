4da1aa45c80480fee67666900a5310bc0b75c30d
rudrashandilya38-cmd <rudrashandilya38@gmail.com>
2026-08-06 05:00:31 +0000

    Understanding datatypes

---
+++ basics/dtypes.js
@@ -0,0 +1,32 @@
+"use strict"; //treat all JS codeas newer version.
+// alert ( 3+3 )// we are using node js, not a browser. So result throws undefined.
+
+console.log( 3+
+    3)// although it is corect, but readability is distorted.n 
+
+    console.log("rishu")
+
+
+let name="rudra" // data type=> string.( because it is written in quotes)
+let age = 18 // data type=> number
+let isLoggedIn= true // boolean type
+
+
+
+
+    // Some data types
+    //1. number=> range 2 to the power 53
+    //2. bigint=> very large numbers
+    //3. string=> "" use double quotes
+    //4. boolean => true/false
+    //5. null=> stand alone value
+    //6. undefined=> suppose you create a variable and did not assign avalue to it. So result is undefined
+    //7. symbol=> to define uniqueness
+
+    //8. object
+
+console.log(typeof null)   // object
+console.log(typeof undefined)   /// undefined.
+
+
+// Using typeof we can determine the data type .
